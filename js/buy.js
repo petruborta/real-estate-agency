8fa0a76f48298e2a968fabcc3dae4197b16a549d
petruborta <petruborta@yahoo.com>
2020-11-01 23:50:51 +0000

    Recreated repository

---
+++ js/buy.js
@@ -0,0 +1,386 @@
+class HouseForSale {
+  constructor(rdc_web_url, address, price, baths, beds, building_size, thumbnail) {
+    var _rdc_web_url = rdc_web_url;
+    var _address = address;
+    var _price = price;
+    var _baths = baths;
+    var _beds = beds;
+    var _building_size = building_size;
+    var _thumbnail = thumbnail;
+
+    this.getRedWebURL = function() { 
+      return _rdc_web_url; 
+    }
+
+    this.getAddress = function() { 
+      return _address; 
+    }
+
+    this.getPrice = function() { 
+      return _price; 
+    }
+
+    this.getBaths = function() { 
+      return _baths; 
+    }
+
+    this.getBeds = function() { 
+      return _beds; 
+    }
+
+    this.getBuildingSize = function() { 
+      return _building_size; 
+    }
+
+    this.getThumbnail = function() { 
+      return _thumbnail; 
+    }
+  }
+}
+
+const searchButton = document.querySelector(".search-houses-btn");
+let housesForSale = [], resultsLimit = 50;
+
+const featuredHouses = document.querySelectorAll(".featured-house").length;
+let featuredHousesPerSlide, slides, slidesToRight, factor;
+
+const previousHouseButton = document.querySelector(".prev-house");
+const nextHouseButton = document.querySelector(".next-house");
+
+const slideshowRect = document.querySelector(".slideshow-container").getBoundingClientRect();
+const slideshowElements = document.querySelector(".slideshow-elements");
+const slideshowElementsRect = slideshowElements.getBoundingClientRect();
+
+const searchResultsContainer = document.querySelector(".search-results-container");
+const housesForSaleContainer = document.querySelector(".houses-for-sale-container");
+const showMoreButton = document.querySelector(".show-more-btn");
+const showAllButton = document.querySelector(".show-all-btn");
+
+updateSlideshowBindings();
+
+window.onresize = () => {
+  updateSlideshowBindings();
+}
+
+function updateSlideshowBindings() {
+  featuredHousesPerSlide = getFeaturedHousesPerSlide();
+  slides = featuredHouses / featuredHousesPerSlide;
+  slidesToRight = slides - 1;
+  factor = window.innerWidth < 1024 ? 1 : 0.5;
+
+  slideshowElements.style.transform = "translateX(0)";
+  previousHouseButton.classList.add("invisible");
+  nextHouseButton.classList.remove("invisible");
+}
+
+function getFeaturedHousesPerSlide() {
+  if (window.innerWidth < 576) {
+    return 1;
+  }
+  if (window.innerWidth < 768) {
+    return 2;
+  }
+  if (window.innerWidth < 1024) {
+    return 3;
+  }
+  
+  return 4;
+}
+
+/*=====================================
+search form functions
+=====================================*/
+searchButton.onclick = listHousesForSale;
+
+function listHousesForSale() {
+  var data = null;
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+      extractDataOfInterest(this.responseText);
+      addHousesToContainer();
+      disableShowMoreAndShowAllButtons(false);
+      makeVisible(searchResultsContainer);
+      scrollToHousesForSale();
+    }
+  });
+
+  xhr.open("GET", "https://realtor.p.rapidapi.com/properties/v2/list-for-sale?price_min=150000&sort=relevance&price_max=500000&sqft_min=1&city=New%20York%20City&limit=50&offset=0&state_code=NY");
+  xhr.setRequestHeader("x-rapidapi-host", "realtor.p.rapidapi.com");
+  xhr.setRequestHeader("x-rapidapi-key", "APIkey");
+
+  xhr.send(data);
+}
+
+function extractDataOfInterest(responseText) {
+  let response = JSON.parse(responseText);
+
+  response.properties.forEach(house => {
+    housesForSale.push(new HouseForSale(
+      house.rdc_web_url,
+      house.address,
+      house.price,
+      house.baths,
+      house.beds,
+      house.building_size,
+      house.thumbnail
+    ))
+  });
+}
+
+function addHousesToContainer() {
+  let visibleHouses = 0;
+  housesForSale.forEach(houseData => {
+    let newHouse = createHouseElement();
+    setHouseImage(newHouse, houseData);
+    setHouseDescription(newHouse, houseData);
+    viewHouseInNewTabOnClick(newHouse, houseData);
+    
+    if (visibleHouses >= 10) {
+      newHouse.classList.add("invisible");
+    }
+
+    housesForSaleContainer.append(newHouse);
+    visibleHouses++;
+  });
+}
+
+
+
+function createHouseElement() {
+  let house = document.createElement("div");
+  house.classList.add("house");
+
+  house.append(createFavoriteElement());
+  house.append(createHouseImageElement());
+  house.append(createHouseDescriptionElement());
+
+  return house;
+}
+
+function createFavoriteElement() {
+  let favorite = document.createElement("div");
+  favorite.classList.add("favorite");
+  favorite.innerHTML = '<i class="far fa-heart"></i>';
+
+  return favorite;
+}
+
+function createHouseImageElement() {
+  let houseImage = document.createElement("div");
+  houseImage.classList.add("house-img");
+
+  return houseImage;
+}
+
+function createHouseDescriptionElement() {
+  let houseDescriptionContainer = document.createElement("div");
+  let houseDescription = document.createElement("div");
+  houseDescriptionContainer.classList.add("house-description-container");
+  houseDescription.classList.add("house-description");
+
+  houseDescription.append(createPriceElement());
+  houseDescription.append(createBedsBathsAreaElement());
+  houseDescription.append(createLineNeighborhoodPostcodeElement());
+  houseDescription.append(createCityCountyStateCodeElement());
+  houseDescriptionContainer.append(houseDescription);
+
+  return houseDescriptionContainer;
+}
+
+function createPriceElement() {
+  let price = document.createElement("span");
+  price.classList.add("price");
+  price.innerHTML = "&#36;";
+
+  return price;
+}
+
+function createBedsBathsAreaElement() {
+  let bedsBathsArea = document.createElement("span");
+  bedsBathsArea.classList.add("beds-baths-area");
+  
+  bedsBathsArea.append(createBedIcon());
+  bedsBathsArea.append(createBathIcon());
+  bedsBathsArea.append(createRulerIcon());
+
+  return bedsBathsArea;
+}
+
+function createBedIcon() {
+  let bed = document.createElement("i");
+  bed.classList.add("fas", "fa-bed");
+  bed.innerText = " Beds";
+
+  return bed;
+}
+
+function createBathIcon() {
+  let bath = document.createElement("i");
+  bath.classList.add("fas", "fa-bath");
+  bath.innerText = " Baths";
+
+  return bath;
+}
+
+function createRulerIcon() {
+  let ruler = document.createElement("i");
+  ruler.classList.add("fas", "fa-ruler-combined");
+
+  return ruler;
+}
+
+function createLineNeighborhoodPostcodeElement() {
+  let lineNeighborhoodPostcode = document.createElement("span");
+  lineNeighborhoodPostcode.classList.add("line-neighborhood-postcode");
+
+  return lineNeighborhoodPostcode;
+}
+
+function createCityCountyStateCodeElement() {
+  let cityCountyStateCode = document.createElement("span");
+  cityCountyStateCode.classList.add("city-county-state-code");
+
+  return cityCountyStateCode;
+}
+
+
+
+function setHouseImage(house, houseData) {
+  let houseImage = house.childNodes[1];
+
+  if (houseData.getThumbnail() != undefined) {
+    houseImage.style.background = `url(${houseData.getThumbnail()})`;
+  }
+}
+
+
+
+function setHouseDescription(houseElement, houseData) {
+  let houseDescriptionContainer = houseElement.childNodes[2];
+  
+  let houseDescription = houseDescriptionContainer.childNodes[0];
+  
+  let price = houseDescription.childNodes[0];
+  let bedsBathsArea = houseDescription.childNodes[1];
+  let lineNeighborhoodPostcode = houseDescription.childNodes[2];
+  let cityCountyStateCode = houseDescription.childNodes[3];
+
+  price.innerHTML += houseData.getPrice();
+
+  bedsBathsArea.childNodes[0].innerText = ` ${houseData.getBeds()} ${bedsBathsArea.childNodes[0].innerText}`;
+  bedsBathsArea.childNodes[1].innerText = ` ${houseData.getBaths()} ${bedsBathsArea.childNodes[1].innerText}`;
+  bedsBathsArea.childNodes[2].innerText = ` ${houseData.getBuildingSize().size} Sq. Ft.`;
+  
+  let address = houseData.getAddress();
+  lineNeighborhoodPostcode.innerText = `${address.line}, ${address.neighborhood_name}, ${address.postal_code}`;
+  cityCountyStateCode.innerText = `${address.city}, ${address.county}, ${address.state_code}`;
+}
+
+
+
+function viewHouseInNewTabOnClick(house, houseData) {
+  house.onclick = function() {
+    window.open(houseData.getRedWebURL());
+  }
+}
+
+
+
+function scrollToHousesForSale() {
+  searchResultsContainer.scrollIntoView({ 
+    behavior: 'smooth' 
+  });
+}
+
+/*=====================================
+featured houses slideshow functions
+=====================================*/
+previousHouseButton.onclick = slideLeft;
+nextHouseButton.onclick = slideRight;
+
+function slideLeft() {
+  slidesToRight += 1;
+  slideshowElements.style.transform = `translateX(calc(-100% * ${factor * (slides - slidesToRight - 1)})`;
+
+  if (atFirstSlide()) {
+    makeInvisible(previousHouseButton);
+  }
+  if (!atLastSlide()) {
+    makeVisible(nextHouseButton);
+  }
+}
+
+function slideRight() {
+  slideshowElements.style.transform = `translateX(calc(-100% * ${factor * (slides - slidesToRight)}))`;
+  slidesToRight -= 1;
+  
+  if (!atFirstSlide()) {
+    makeVisible(previousHouseButton);
+  }
+  if (atLastSlide()) {
+    makeInvisible(nextHouseButton);
+  }
+}
+
+function atFirstSlide() {
+  return slidesToRight + 1 == slides;
+}
+
+function atLastSlide() {
+  return slidesToRight <= 0;
+}
+
+function makeInvisible(element) {
+  element.classList.add("invisible");
+}
+
+function makeVisible(element) {
+  element.classList.remove("invisible");
+}
+
+/*=====================================
+search results functions
+=====================================*/
+showMoreButton.onclick = showMoreHouses;
+showAllButton.onclick = showAllHouses;
+
+function showMoreHouses() {
+  let invisibleHouses = document.querySelectorAll(".house.invisible");
+
+  if (invisibleHouses.length < 10) {
+    n = invisibleHouses.length;
+  }
+  if (invisibleHouses.length > 0) {
+    for (let i = 0; i < 10; ++i) {
+      invisibleHouses[i].classList.remove("invisible");
+    }
+  } else {
+    disableShowMoreAndShowAllButtons(true);
+  }
+}
+
+function showAllHouses() {
+  let invisibleHouses = document.querySelectorAll(".house.invisible");
+
+  invisibleHouses.forEach(house => {
+    house.classList.remove("invisible");
+  });
+
+  disableShowMoreAndShowAllButtons(true);
+}
+
+function disableShowMoreAndShowAllButtons(value) {
+  showMoreButton.disabled = value;
+  showAllButton.disabled = value;
+
+  if (value) {
+    showMoreButton.classList.add("disabled");
+    showAllButton.classList.add("disabled");
+  } else {
+    showMoreButton.classList.remove("disabled");
+    showAllButton.classList.remove("disabled");
+  }
+}
---
+++ js/main.js
@@ -0,0 +1,249 @@
+const responses = [
+  "What can I help you with?",
+  "One moment, please!",
+  "Can you elaborate your question a little bit more, please?",
+  "Glad I could help you!",
+  "Sorry, I don't have an asnwer for this."
+];
+
+const mainBar = document.querySelector(".header-main-bar");
+const mainNav = document.querySelector(".header-main-nav");
+const headerAccountButton = document.querySelector(".header-account");
+const headerChatButton = document.querySelector(".header-chat");
+const menuButton = document.querySelector(".header-menu-btn")
+
+const accountTooltip = document.querySelector(".account-tooltip");
+const accountTooltipSignUpButton = document.querySelector(".signup-btn");
+const accountTooltipSignInButton = document.querySelector(".signin-btn");
+const signUpForm = document.querySelector(".signup-form");
+const signInForm = document.querySelector(".signin-form");
+
+const chat = document.querySelector(".chat-container");
+const chatMessages = document.querySelector(".chat-messages");
+const chatUserInput = document.querySelector(".user-input");
+const sendMessageButton = document.querySelector(".send-message-btn");
+
+const exploreButton = document.querySelector(".explore-btn");
+
+const guideLinks = document.querySelectorAll(".guide-link");
+
+const moreMarketsButton = document.querySelector(".more-markets-btn");
+const moreStatesButton = document.querySelector(".more-states-btn");
+
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
+
+window.onclick = (event) => {
+  if (event.target != headerAccountButton && accountTooltip.classList.contains("visible")) {
+    accountTooltip.classList.remove("visible");
+  }
+  if (event.target == signUpForm) {
+    signUpForm.style.display = "none";
+  }
+  if (event.target == signInForm) {
+    signInForm.style.display = "none";
+  }
+}
+
+window.onresize = () => {
+  if (window.innerWidth >= 1024 && mainNav.classList.contains("visible")) {
+    mainNav.classList.remove("visible");
+    menuButton.classList.remove("open");
+  }
+}
+
+/*=====================================
+header functions
+=====================================*/
+menuButton.addEventListener("click", () => {
+  menuButton.classList.toggle("open");
+  mainNav.classList.toggle("visible");
+});
+
+if (exploreButton != null) {
+  exploreButton.addEventListener("click", () => {
+    document.querySelector('.main-page').scrollIntoView({ 
+      behavior: 'smooth' 
+    });
+  }); 
+}
+
+headerAccountButton.addEventListener("click", () => {
+  accountTooltip.classList.toggle("visible")
+});
+
+function showSignupForm() {
+  signUpForm.firstElementChild.classList.remove("zoom-out");
+  signUpForm.style.display="block";
+}
+
+function hideSignupForm() {
+  signUpForm.firstElementChild.classList.add("zoom-out");
+  setTimeout(() => { 
+    signUpForm.style.display="none"; 
+  }, 300);
+}
+
+accountTooltipSignUpButton.addEventListener("click", () => {
+  showSignupForm();
+});
+
+function showSigninForm() {
+  signInForm.firstElementChild.classList.remove("zoom-out");
+  signInForm.style.display = "block";
+}
+
+function hideSigninForm() {
+  signInForm.firstElementChild.classList.add("zoom-out");
+  setTimeout(() => { 
+    signInForm.style.display="none"; 
+  }, 300);
+}
+
+accountTooltipSignInButton.addEventListener("click", () => {
+  showSigninForm();
+});
+
+headerChatButton.addEventListener("click", () => {
+  chat.classList.toggle("visible");
+});
+
+function closeChat() {
+  chat.classList.remove("visible");
+}
+
+function getUserInputValue() {
+  return chatUserInput.value;
+}
+
+function clearUserInput() {
+  chatUserInput.value = "";
+}
+
+function createMessage() {
+  let userInput = getUserInputValue();
+  if (!userInput) {
+    return null;
+  }
+
+  let newMessage = document.createElement("p");
+  newMessage.classList.add("message");
+  newMessage.innerText = userInput;
+
+  return newMessage;
+}
+
+function createResponse() {
+  let newResponse = document.createElement("p");
+  newResponse.classList.add("response", "message");
+  newResponse.innerText = responses[Math.floor(Math.random() * 5)];
+
+  return newResponse;
+}
+
+function addMessage(message) {
+  chatMessages.append(message);
+}
+
+function scrollToChatBottom() {
+  chatMessages.scrollTo(0, chatMessages.scrollHeight);
+}
+
+function sendMessage() {
+  let message = createMessage();
+  if (message == null) {
+    return false;
+  }
+
+  addMessage(message);
+  clearUserInput();
+  scrollToChatBottom();
+
+  return true;
+}
+
+function sendResponse() {
+  let response = createResponse();
+  addMessage(response);
+  scrollToChatBottom();
+}
+
+sendMessageButton.addEventListener("click", () => {
+  if (sendMessage()) {
+    setTimeout(() => {
+      sendResponse();
+    }, 500);
+  }
+});
+
+/*=====================================
+main page functions
+=====================================*/
+function headerIsInViewport() {
+  const rect = document.querySelector(".site-header").getBoundingClientRect();
+  return (
+    rect.height + rect.top  >= 110
+  );
+}
+
+function mainPageIsInViewport() {
+  const rect = document.querySelector(".main-page").getBoundingClientRect();
+  return (
+    window.innerHeight - rect.top  <= 25
+  );
+}
+
+document.addEventListener("scroll", () => {
+  mainBar.style.opacity = (!headerIsInViewport()) ? "0" : "1";
+  if (exploreButton != null) {
+    exploreButton.style.opacity = (!mainPageIsInViewport()) ? "0" : "1";
+  }
+});
+
+/*=====================================
+guides section functions
+=====================================*/
+guideLinks.forEach(guideLink => {
+  guideLink.addEventListener("mouseenter", () => {
+    let guideName = guideLink.classList[1];
+    let guidesImgContainer = document.querySelector(".guides-img-container");
+    let bgURL = `url(images/${guideName}-bg-sm.jpg)`;
+    
+    if (guidesImgContainer.style.backgroundImage == bgURL) {
+      return;
+    }
+
+    if (guideName == "sell") {
+      guidesImgContainer.style.backgroundPosition = "75%";
+    } else {
+      guidesImgContainer.style.backgroundPosition = "center";
+    }
+
+    guidesImgContainer.style.backgroundImage = bgURL;
+  });
+});
+
+/*=====================================
+footer functions
+=====================================*/
+function expandList(element) {
+  element.classList.toggle("expanded");
+}
+
+function changeButtonText(button) {
+  if (button.innerText == "Show more") {
+    button.innerText = "Show less";
+  } else {
+    button.innerText = "Show more";
+  }
+}
+
+moreMarketsButton.addEventListener("click", () => {
+  expandList(document.querySelector(".popular-markets-ul"));
+});
+
+moreStatesButton.addEventListener("click", () => {
+  expandList(document.querySelector(".popular-states-ul"));
+});
